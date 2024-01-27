83e6c35bebea5697681a1461422315c84e779efe
Dieter Bergmann <dieterbergmann@arcor.de>
2024-01-27 14:28:29 +0100

    Initial Commit

---
+++ WordGarden-SwiftUI/WordGarden_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUIApp.swift
+//  WordGarden-SwiftUI
+//
+//  Created by Dieter Bergmann on 27.01.24.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
